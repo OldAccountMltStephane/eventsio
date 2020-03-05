fea6e4c5e8230a9151947b7e5cd04219f81f9b5d
StephDw <spmulot@gmail.com>
2020-03-05 15:05:26 +0100

    Add install script

--- setup.sh
+++ setup.sh
@@ -1,3 +1,13 @@
+chown -R $USER:www-data storage
+chown -R $USER:www-data bootstrap/cache
+chown -R $USER:www-data database
+chown -R $USER:www-data database/database/sqlite.sqlite
+
+chmod -R 775 storage
+chmod -R 775 bootstrap/cache
+chmod -R 775 database
+chmod -R 775 database/database/sqlite.sqlite
+
 composer install
 npm install
 npm run dev
