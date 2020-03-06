b058bb91471126348cf81cd374e2138f9b0e0057
StephDw <spmulot@gmail.com>
2020-03-06 16:52:08 +0100

    Add mobile app download link

--- resources/views/home/home.blade.php
+++ resources/views/home/home.blade.php
@@ -15,7 +15,7 @@
     </div>
 </div>
 
-<div class="container bg-white rounded-0 border p-3">
+<div class="container bg-white rounded-0 border p-3 mb-4">
     <div class="row text-center">
         <div class="col-sm my-3"><span class="font-weight-bold text-uppercase">Evenements à venir</span>
             <hr class="my-1">{{ $evenements->count() }}</div>
@@ -24,6 +24,16 @@
         <div class="col-sm my-3"><span class="font-weight-bold text-uppercase">Participations à une session</span>
             <hr class="my-1">{{ $participations->count() }}</div>
     </div>
+</div>
+
+<div class="container bg-white rounded-0 border p-3">
+    <h1>
+        <a href="https://github.com/MltStephane/eventsio/blob/master/Pointage/Pointage/Exe/Pointage.apk">
+            Télécharger l'application mobile
+        </a>
+    </h1>
+    <p>L'application mobile vous permet de scanner les codes barres de vos invités ou d'acceder plus rapidement aux
+        codes barres des événements auxquels vous participez</p>
 </div>
 
 @endsection
